6bf8d8988992f539a996f5ef297836a6ecba60da
snake <adsloader99@gmail.com>
2021-09-07 10:09:01 +0900

    .

--- app/src/main/java/com/psw/quick/compose/MainActivity.kt
+++ app/src/main/java/com/psw/quick/compose/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.psw.quick.compose
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,80 +33,91 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun TextOut(message: String) {
-    Text(
-        modifier = Modifier
-            .fillMaxWidth()             // width
-            .background(Color.Red),     // 백그라운드
-        color = Color(android.graphics.Color.parseColor("#efefef") ),             // 색상
-        text  = "$message!",         // text
-        textAlign = TextAlign.Center    // Align
-    )
-}
-
-@Composable
-fun testMain(){
-    basic_layout()
-}
-
-@Composable
-private fun basic_layout() {
-    // linearlayout과 유사 orientation vertical
-    Column(
-        Modifier
-            .background(Color.Blue)
-            .fillMaxWidth()    // %를 설정하여 채우기를 조절가능
-            .wrapContentHeight(),  // %를 설정하여 채우기를 조절가능
-        horizontalAlignment = Alignment.CenterHorizontally, // Alignment이다.
-        verticalArrangement = Arrangement.Center            // Arrangement이다.
-    ) {
-        // Basic
-        Image(
-            painter = rememberImagePainter("https://image.api.playstation.com/vulcan/ap/rnd/202103/0200/9RHJbZ83bo1d61vdHe9NWxhl.png"),
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        // Advanced
-        Image(
-            painter = rememberImagePainter(
-                data = "https://image.api.playstation.com/vulcan/ap/rnd/202103/0200/9RHJbZ83bo1d61vdHe9NWxhl.png",
-                builder = {
-                    transformations(CircleCropTransformation())
-                }
-            ),
-            contentDescription = null,
-            modifier = Modifier
-                .size(84.dp)
-                .padding(16.dp)
-        )
-        TextOut("hi, this is test")
-        Divider(
-            color = Color.Transparent,
-            thickness = 10.dp
-        ) // 구분선
-        TextOut("Next line")
-
+    @Composable
+    private fun basic_layout() {
+        // linearlayout과 유사 orientation vertical
         Column(
             Modifier
-                .background(Color.Yellow)
-                // margin이 없다. padding을 두번 활용해야 한다.
-                .padding(top = 20.dp, start = 16.dp, end = 16.dp, bottom = 20.dp)
-                .fillMaxHeight(0.4f)     // %를 설정하여 채우기를 조절가능
+                .background(Color.Blue)
+                .fillMaxWidth()    // %를 설정하여 채우기를 조절가능
+                .wrapContentHeight(),  // %를 설정하여 채우기를 조절가능
+            horizontalAlignment = Alignment.CenterHorizontally, // Alignment이다.
+            verticalArrangement = Arrangement.Center            // Arrangement이다.
         ) {
-            TextOut("2. basic_layout2")
+            // Basic
+            Image(
+                painter = rememberImagePainter("https://image.api.playstation.com/vulcan/ap/rnd/202103/0200/9RHJbZ83bo1d61vdHe9NWxhl.png"),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Advanced
+            Image(
+                painter = rememberImagePainter(
+                    data = "https://image.api.playstation.com/vulcan/ap/rnd/202103/0200/9RHJbZ83bo1d61vdHe9NWxhl.png",
+                    builder = {
+                        transformations(CircleCropTransformation())
+                    }
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(84.dp)
+                    .padding(16.dp)
+            )
+            TextOut("hi, this is test")
+            Divider(
+                color = Color.Transparent,
+                thickness = 10.dp
+            ) // 구분선
+            TextOut("Next line")
+
+            Row(
+                Modifier
+                    .background(Color.Yellow)
+                    // margin이 없다. padding을 두번 활용해야 한다.
+                    .padding(top = 20.dp, start = 16.dp, end = 16.dp, bottom = 20.dp)
+                    .fillMaxHeight(0.4f)     // %를 설정하여 채우기를 조절가능
+            ) {
+                ButtonMake(title = "clcik me", {})
+            }
         }
     }
-}
 
+    @Composable
+    fun TextOut(message: String) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()             // width
+                .background(Color.Red),     // 백그라운드
+            color = Color(android.graphics.Color.parseColor("#efefef") ),             // 색상
+            text  = "$message!",         // text
+            textAlign = TextAlign.Center    // Align
+        )
+    }
+
+    @Composable
+    fun ButtonMake ( title : String, onClick : () -> Unit = {}){
+        Button(onClick = onClick) {
+            Text(
+                title,
+                color = Color.Gray
+            )
+        }
+    }
+
+    @Composable
+    fun testMain(){
+        basic_layout()
+    }
+
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     QuickComposeTutorialTheme {
-        testMain()
+        //testMain()
     }
 }
